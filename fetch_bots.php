6c7087b7309ec24248897ef642076d6af0ecd031
Ethan Kent-Ogiata <113203613+EthanKent12@users.noreply.github.com>
2025-03-14 08:31:17 -0700

    Add files via upload

--- fetch_bots.php
+++ fetch_bots.php
@@ -1,16 +1,16 @@
 <?php
-require 'db.php';
+header('Content-Type: application/json');
 
-$sql = "SELECT * FROM product";
-$stmt = $conn->prepare($sql);
-$stmt>execute();
-$result = $stmt->get_result();
+// Fake bot data
+$bots = [
+    [ "id" => 1, "name" => "Pirate Bot", "tests" => 30, "status" => "Success" ],
+    [ "id" => 2, "name" => "Tim Bot", "tests" => 10, "status" => "Fail" ],
+    [ "id" => 3, "name" => "Phillip Bot", "tests" => 12, "status" => "Success" ],
+    [ "id" => 4, "name" => "BluePeak IT AI", "tests" => 22, "status" => "Success" ],
+    [ "id" => 5, "name" => "AI Assistant", "tests" => 25, "status" => "Fail" ],
+    [ "id" => 6, "name" => "Nate Bot", "tests" => 5, "status" => "Fail" ]
+];
 
-$bots = [];
-
-while ($row = $result->fetch_assoc()) {
-    $bots[] = $row;
-}
-
+// Convert to JSON and send response
 echo json_encode($bots);
 ?>
